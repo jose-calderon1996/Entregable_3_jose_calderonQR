1f2f5c29f6cb909f45935736c9a227149b9f468e
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2024-11-20 15:38:47 -0300

    prueba unitaria restablecer contraseña

--- src/app/restablecer-contrasena/restablecer-contrasena.page.spec.ts
+++ src/app/restablecer-contrasena/restablecer-contrasena.page.spec.ts
@@ -1,11 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RestablecerContrasenaPage } from './restablecer-contrasena.page';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 describe('RestablecerContrasenaPage', () => {
   let component: RestablecerContrasenaPage;
   let fixture: ComponentFixture<RestablecerContrasenaPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [RestablecerContrasenaPage],
+      providers: [
+        { provide: AngularFireAuth, useValue: {} }, // Mock básico para AngularFireAuth
+        { provide: FIREBASE_OPTIONS, useValue: {} }, // Mock básico para FIREBASE_OPTIONS
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RestablecerContrasenaPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
