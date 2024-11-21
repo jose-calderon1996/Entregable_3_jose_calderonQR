16c4982d48577271fa44a9ec4a7067f31f659a74
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2024-11-21 14:35:44 -0300

    prueba unitaria escaner qr

--- src/app/scanner-qr/scanner-qr.page.spec.ts
+++ src/app/scanner-qr/scanner-qr.page.spec.ts
@@ -1,11 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
 import { ScannerQrPage } from './scanner-qr.page';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { environment } from '../../environments/environment'; // Asegúrate de que la ruta sea correcta.
 
 describe('ScannerQrPage', () => {
   let component: ScannerQrPage;
   let fixture: ComponentFixture<ScannerQrPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ScannerQrPage],
+      imports: [
+        IonicModule.forRoot(),
+        AngularFireModule.initializeApp(environment.firebaseConfig) // Configuración de Firebase
+      ],
+      providers: [
+        { provide: AngularFireAuth, useValue: {} } // Mock del servicio AngularFireAuth
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ScannerQrPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
