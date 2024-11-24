85abd6a3e6939389ef7453c42aff91e24ce13a8b
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2024-11-23 21:37:10 -0300

    funcionalidad apra cambiar estado del  boton de login y cerrar seion cuando usuario este logeado on no desaparezca

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,31 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
-  color: string = 'primary';
+export class HomePage implements OnInit {
+  isLoggedIn: boolean = false; // Estado para alternar los botones
 
-  constructor(private navCtrl: NavController, private authService: AuthService) {}
+  constructor(
+    private navCtrl: NavController,
+    private authService: AuthService,
+    private afAuth: AngularFireAuth 
+  ) {}
 
+  ngOnInit() {
+    this.checkAuthenticationStatus(); // Escuchar los cambios de autenticación al cargar la página
+  }
+
+  // Método para verificar si el usuario está autenticado para luego cambiar estado de boton de login a cerar sesion
+  checkAuthenticationStatus() {
+    this.afAuth.onAuthStateChanged((user) => {
+      this.isLoggedIn = !!user; // Si hay un usuario, isLoggedIn es true; si no, false
+      
+    });
+  }
+
+  // Navegar al registro estudiantil
   navegarRegistro() {
     this.navCtrl.navigateForward('/registro-estudiantil');
   }
 
+  // Navegar a la página de login
   navigateToLogin() {
     this.navCtrl.navigateForward('/login');
   }
+
+  // Navegar al perfil del usuario
   navigateperfil() {
     this.navCtrl.navigateForward('/perfil');
   }
+
+  // Navegar al escáner QR
   navigateQR() {
     this.navCtrl.navigateForward('/scanner-qr');
   }
 
-  
+  // Método para cerrar sesión
+  async cerrarSesion() {
+    await this.authService.logout(); // Cierra sesión en Firebase
+    console.log('Sesión cerrada');
+  }
 }
